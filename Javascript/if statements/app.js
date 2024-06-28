13146061b10f5edf93d5169facbd99e559c54d99
dheeraj jadaun <dheerajjadaun2000@gmail.com>
2024-06-28 18:50:58 +0530

    if statement

---
+++ Javascript/if statements/app.js
@@ -0,0 +1,9 @@
+ console.log("Before Conditional")
+ let random=Math.random();
+ 
+ if(random<0.5){
+    console.log("YOUR NUMBER IS LESS THAN O.5")
+    console.log(random)
+ }
+
+ console.log("after Conditional")

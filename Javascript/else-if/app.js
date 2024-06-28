d23b52e80fb53c5c6d207e799948cc83f946a305
dheeraj jadaun <dheerajjadaun2000@gmail.com>
2024-06-28 19:22:06 +0530

    else if

---
+++ Javascript/else-if/app.js
@@ -0,0 +1,25 @@
+// const dayOfWeek='Friday';
+
+// if(dayOfWeek==='Monday'){
+//     console.log("UGHH I HATE MONDAY")
+// }
+
+// else if(dayOfWeek==='Saturday'){
+//     console.log("YAY I LOVE SATURDAY")
+// }
+// else if(dayOfWeek==='Friday'){
+//     console.log("FRIDAYS ARE DECENT. ESPECIALLY AFTER WORK")
+// }
+
+
+const age = 8;
+
+if(age<5){
+    console.log("You are a baby, You get it for free")
+}else if(age<10){
+    console.log("You are a child, You pay $10")
+}else if(age<65){
+    console.log("You are an adult,You pay $20")
+}else if(age>65{
+    console.log("You are a senior citizen,You pay $10")
+}

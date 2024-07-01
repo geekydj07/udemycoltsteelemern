58f914ea56f2a441d7d7bd5540323b98bae627d4
dheeraj jadaun <dheerajjadaun2000@gmail.com>
2024-07-01 17:44:34 +0530

    switch statement

---
+++ Javascript/switch statement/app.js
@@ -0,0 +1,18 @@
+const day=2;
+
+switch(day){
+    case 1:
+        console.log("MONDAY")
+    case 2:
+        console.log("TUESDAY")
+    case 3:
+        console.log("WEDNESDAY")
+    case 4:
+        console.log("THURSDAY")
+    case 5:
+        console.log("FRIDAY")
+    case 6:
+        console.log("SATURDAY")
+    case 7:
+        console.log("SUNDAY")
+}

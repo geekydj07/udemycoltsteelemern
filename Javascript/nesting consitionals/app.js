6c0a69a03470768642e10b967afb173b9bdc6ad2
dheeraj jadaun <dheerajjadaun2000@gmail.com>
2024-07-01 15:50:27 +0530

    nesting conditionals

--- Javascript/nesting consitionals/app.js
+++ Javascript/nesting consitionals/app.js
@@ -1,8 +1,12 @@
 const password=prompt("please enter anew password")
 
 //password must be 6 characters
-if(password.length>=6){
-    console.log("LONG ENOUGH PASSWORD")
+if(password.length >= 6){
+    if(password.indexOf(' ')===-1){
+        console.log("Valid password")
+    }else{
+        console.log("it cannot contain space")
+    }
 }else{
     console.log("password too short")
 }
@@ -10,8 +14,3 @@ if(password.length>=6){
 
 //do not include space
 
-if(password.indexOf(' ')===-1){
-    console.log("Good job! No space")
-}else{
-    console.log("it cannot contain space")
-}
